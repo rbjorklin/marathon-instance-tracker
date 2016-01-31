e0dea9765c327b5bf00be77a4e4120bf536a9cf3
Robin Björklin <robin.bjorklin@gmail.com>
2016-01-31 17:20:47 +0100

    Added break in getMyId.sh

--- getMyId.sh
+++ getMyId.sh
@@ -4,6 +4,7 @@ idCounter=1
 for taskId in $(zkcli --servers srv-1,srv-2,srv-3 get /instance-tracker/${MARATHON_APP_ID} | tr , " ") ; do
     if [[ "${MESOS_TASK_ID}" == "${taskId}" ]] ; then
         echo ${idCounter}
+        break
     fi
     idCounter=$((idCounter + 1))
 done
